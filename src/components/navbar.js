298927c28a252215387887922d38f12d701514b9
TylenolSniffinTyler <willythewhale8@gmail.com>
2023-02-23 22:50:42 -0500

    userState update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,38 @@
+import { Link } from "react-router-dom";
+import { useUserContext } from "../hooks/useUserContext";
 import logo from "../public/LogoChat.png"
-
+import userPFP from "../public/user.png"
+import logpng from "../public/logout.png"
+import cog from "../public/cog.png"
+import { useLogout } from "../hooks/useLogout";
+import { useState } from "react";
 
 const Navbar = () => {
+    const {logout} = useLogout()
+    const {user} = useUserContext(); 
+    const [pfpPopUp, setPfpPopUp] = useState(false)
     return ( <div className="navbar">
         <img className ="logo" src={logo} />
         <nav className="navComponents">
-             <div>Home</div>
-             <div>Log in</div>
+             <Link to="/">Home</Link>
+             {!user && (<Link to="/login">Log in</Link>)}
              <div>Contact</div>
              <div>About</div>
              <div>FQA</div>
+             {user && (<div className="userPFP" onClick={()=>setPfpPopUp(!pfpPopUp)}> 
+                <h1 className="username"> {user._doc.username} </h1>
+                {pfpPopUp && <section className="pfpPopUp"> 
+                    <div className="popUptext"> Account Settings </div>
+                    <hr/>
+                    <div className="popUptext" id="friendcode" onClick={()=>{
+                        navigator.clipboard.writeText(user._doc.friendCode)
+                        alert("Copied Friend Code")
+                    }}>Friend Code: {user._doc.friendCode}</div>
+                    <hr/>
+                    <div className="popUptext" onClick={logout}>Logout</div><img src={logpng}/>
+                </section>}
+                <img src={userPFP} className="pfp"/>
+             </div>)}
         </nav>
     </div> );
 }
