2b78590f5dad7a6153b47bcd5b61d583e97fa5ab
Wilson <willythewhale8@gmail.com>
2023-03-05 17:32:22 -0500

    Second Big Merge

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useFetch} from "../hooks/useFetch"
 import { useUserContext } from "../hooks/useUserContext";
 import Friend from "./friend";
-import { useState } from "react";
 import FriendRequest from "./friendRequest";
 import { useActiveContext } from "../hooks/userActiveContext";
 
@@ -11,13 +10,12 @@ const HomePage = () => {
     const [addFriendPopUp, setAddFriendPopUp] = useState(false)
     const [friendCode, setFriendCode] = useState(null)
     const {isPending, data, fetchData, error} = useFetch()
-  
-
-
+    const {activeComponent} = useActiveContext()
+    console.log( user._doc.friendRequest.length );
     return (<div className="homePage">
             <section className="friendsBar"> 
-                <div onClick={()=>{setFriendPopUp(!friendPopUp)}}> <h1>Friends </h1> <div className="addFriendLogo" onClick={() => {setAddFriendPopUp(!addFriendPopUp)}}/></div>
-                {addFriendPopUp && <form className="addFriendPopUp" onSubmit={(e)=>{
+                <div onClick={()=>{setAddFriendPopUp(!addFriendPopUp)}}> <h1>Friends </h1> <div className= { user._doc.friendRequest.length >= 1 ? "addFriendLogo notif" : "addFriendLogo"} onClick={() => {setAddFriendPopUp(!addFriendPopUp)}}/></div>
+                {addFriendPopUp && <form className= "addFriendPopUp" onSubmit={(e)=>{
                     e.preventDefault()
                     fetchData(`/chatAPI/user/sendRequest/${user._doc.email}/${friendCode}`)
                     console.log(data)
@@ -25,10 +23,10 @@ const HomePage = () => {
                 <div className="codeInput"><input type="text"  value = {friendCode} onChange={(input) => {setFriendCode(input.target.value)}}/> </div> 
             </form>}
             {data && <div> Sent!</div>}
-                {friendPopUp && <FriendRequest />}
+                {addFriendPopUp && <FriendRequest />}
                 {user._doc.friends.map(friend => (<Friend user = {friend}/>))}
             </section>    
-            {activeComponent == null && <section className="chatSection"> Not Active </section>}        
+            {activeComponent == null && <section className="chatSection inactive"> <div className="inactivePic"/> <div>Start a conversation with a friend! </div> <span onClick={() => {setAddFriendPopUp(!addFriendPopUp)} }> or Add a friend</span> </section>}        
     </div>);
 }
  
